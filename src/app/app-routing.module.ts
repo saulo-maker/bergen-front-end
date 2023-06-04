a8f03fd14ce7d59073f99fd14b2fe8cd5838d1cf
saulo.maker <saulosousasilva3@gmail.com>
2023-06-04 16:42:49 -0300

    Upload projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { MainComponent } from './components/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainComponent
+  },
+  {
+    path: 'login', component: NavComponent
+  },
+  {
+    path: 'cliente', component: ClienteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { NavComponent } from './components/nav/nav.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { MainComponent } from './components/main/main.component';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ClienteComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    PasswordModule,
+    FormsModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
